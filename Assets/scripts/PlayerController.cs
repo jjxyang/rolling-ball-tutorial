5e2e34f93937b2083f0dc47f2140f724d9ed84f3
Jessie Yang <jessiejxyang@outlook.com>
2017-07-06 22:45:39 -0700

    Finish Unity Roll-a-Ball tutorial up to Step 3.01 (Creating Collectable Objects)

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed; // public --> shows up in inspector as editable property
+    private Rigidbody rb;
+
+    // called on first frame that script is active
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // performed before every frame calculation
+    void Update()
+    {
+        
+    }
+
+    // performed before every physics calculation
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+}
