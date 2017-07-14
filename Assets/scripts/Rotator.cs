1873af2653f4745d753d6df4dcfba42f249e796a
Jessie Yang <jessiejxyang@outlook.com>
2017-07-14 00:20:34 -0700

    Translate all Pickup objects back and forth by a constant amount

--- Assets/scripts/Rotator.cs
+++ Assets/scripts/Rotator.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour {
+    private float maxVal;
+    private float minVal;
+    private float distance;
 
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+        distance = 1.5f;
+        maxVal = transform.position.x + distance;
+        minVal = transform.position.x - distance;
+    }
+
+    // Update is called once per frame
+    void Update() {
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.position = new Vector3(Mathf.PingPong(Time.time * 2, maxVal - minVal) + minVal,
+                                                        transform.position.y,
+                                                        transform.position.z);
 	}
 }
